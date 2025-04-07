d193d2c8eee0c09c66aefa39ca14ee52761257d5
aabouqas42 <abouqassimeledrissi@gmail.com>
2025-04-07 10:40:03 +0100

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Defining read file function """
 
+
 def read_file(filename=""):
-    """ call open functions to open the file, this operation will be able us to make operations to a file like read, write"""
+    """ call open functions to open the file"""
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
