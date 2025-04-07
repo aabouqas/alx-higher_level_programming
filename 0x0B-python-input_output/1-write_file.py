51e0af5f35155c5c7b9c1bd82704ef70afa205c0
aabouqas42 <abouqassimeledrissi@gmail.com>
2025-04-07 10:58:09 +0100

    done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""""""
+""" Defining write file function """
 
 
 def write_file(filename="", text=""):
+    """ call open functions to open the file"""
     with open(filename, "w", encoding="utf-8") as file:
         count = file.write(text)
         return count
